ee73472af2ef181e5e767a4bf924fe871f6ce98d
Lndael <42750761+Lndael@users.noreply.github.com>
2019-02-14 17:51:32 +0300

    Add files via upload

--- XUiBot.py
+++ XUiBot.py
@@ -1,30 +1,31 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import random
-randint = random.randint(0, 1000000)
+
 def write_msg(user_id, random_id, message):
-    vk_session.method('messages.send', {'user_id': user_id, 'random_id': randint, 'message': message})
+    vk_session.method('messages.send', {'user_id': user_id, 'random_id': randomint, 'message': message})
 
 vk_session = vk_api.VkApi(token = 'd4457584837a3418117a2cae708cad50d8f2ecd708c6fc56351e24d22d9aeace09e2af75551ce774ce687')
 vk_session._auth_token()
 vk = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
-
-while true:
+while True:
+    
+    longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
+        randomint = random.randint(0, 1000000)
     # Если пришло новое сообщение
         if event.type == VkEventType.MESSAGE_NEW:
     
         # Если оно имеет метку для меня( то есть бота)
             if event.to_me:
-        
+                
             # Сообщение от пользователя
                 request = event.text
-            
-            # Каменная логика ответа
+                
                 if request == "привет":
-                write_msg(event.user_id, randint, "Хай")
+                 write_msg(event.user_id, randomint, "Хай")
                 elif request == "пока":
-                write_msg(event.user_id, randint, "Пока((")
+                 write_msg(event.user_id, randomint, "Пока(")
                 else:
-                write_msg(event.user_id, randint, "Не поняла вашего ответа...")
+                 write_msg(event.user_id, randomint, "Не понял вашего ответа...")
+
